8ae6d023374ab3ae96a08d9b6c86fb2c15ae077c
Mikhail Sokolov <alienff@gmail.com>
2016-01-20 14:25:48 +0300

    Load data via ajax

--- client/js/js.js
+++ client/js/js.js
@@ -1,16 +1,28 @@
+var map;
+var layers = [];
+
 function initMap() {
-    var map = L.map('map')
+    map = L.map('map')
         .setView([0, 0], 0);
     L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
         maxZoom: 19
     }).addTo(map);
+}
 
-    var data = [];
-    var latlngs = [];
-    for (var i = 0; i < data.length; i++) {
-        var point = data[i];
-        latlngs.push(L.latLng(point.lat, point.lon, point.altitude));
-    }
-    var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+function loadData(url) {
+    $.ajax(url, {
+        success: function(data) {
+            var latlngs = [];
+            for (var i = 0; i < data.length; i++) {
+                var point = data[i];
+                latlngs.push(L.latLng(point.lat, point.lon, point.altitude));
+            }
+            $.each(layers, function(index, layer) {
+                map.removeLayer(layer);
+            });
+            var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+            layers.push(polyline);
+        }
+    });
 }
